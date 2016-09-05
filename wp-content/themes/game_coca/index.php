1b7d07e0ffa4c5711b225bce5f37f944f9927e74
congtrieu <trieu.dinh@digipencil.vn>
2016-09-05 19:06:15 +0700

    update form

--- wp-content/themes/game_coca/index.php
+++ wp-content/themes/game_coca/index.php
@@ -55,73 +55,11 @@
     </header>
     <main>
         <div class="container">    
-            <div id="loginbox" style="display:none;margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">                    
-                <div class="panel panel-info" >
-                    <div class="panel-heading">
-                        <div class="panel-title">Sign In</div>
-                        <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="#">Forgot password?</a></div>
-                    </div>     
-
-                    <div style="padding-top:30px" class="panel-body" >
-
-                        <div style="display:none" id="login-alert" class="alert alert-danger col-sm-12"></div>
-
-                        <form id="loginform" class="form-horizontal" role="form">
-
-                            <div style="margin-bottom: 25px" class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                                <input id="login-username" type="text" class="form-control" name="username" value="" placeholder="username or email">                                        
-                            </div>
-
-                            <div style="margin-bottom: 25px" class="input-group">
-                                <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                                <input id="login-password" type="password" class="form-control" name="password" placeholder="password">
-                            </div>
-
-
-
-                            <div class="input-group">
-                                <div class="checkbox">
-                                    <label>
-                                        <input id="login-remember" type="checkbox" name="remember" value="1"> Remember me
-                                    </label>
-                                </div>
-                            </div>
-
-
-                            <div style="margin-top:10px" class="form-group">
-                                <!-- Button -->
-
-                                <div class="col-sm-12 controls">
-                                    <a id="btn-login" href="#" class="btn btn-success">Login  </a>
-                                    <a id="btn-fblogin" href="#" class="btn btn-primary">Login with Facebook</a>
-
-                                </div>
-                            </div>
-
-
-                            <div class="form-group">
-                                <div class="col-md-12 control">
-                                    <div style="border-top: 1px solid#888; padding-top:15px; font-size:85%" >
-                                        Don't have an account! 
-                                        <a href="#" onClick="$('#loginbox').hide(); $('#signupbox').show()">
-                                            Sign Up Here
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>    
-                        </form>     
-
-
-
-                    </div>                     
-                </div>  
-            </div>
-            <div id="signupbox" style="margin-top:50px" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+           
+            <div id="signupbox" style="margin-top:50px" class="mainbox col-md-9 col-md-offset-1 col-sm-8 col-sm-offset-2">
                 <div class="panel panel-info">
                     <div class="panel-heading">
-                        <div class="panel-title">Sign Up</div>
-                        <div style="float:right; font-size: 85%; position: relative; top:-10px"><a id="signinlink" href="#" onclick="$('#signupbox').hide(); $('#loginbox').show()">Sign In</a></div>
+                        <div class="panel-title">Đăng ký</div>
                     </div>  
                     <div class="panel-body" >
                         <form id="signupform" class="form-horizontal" role="form">
@@ -131,27 +69,27 @@
                                 <span></span>
                             </div>
                             <div class="form-group">
-                                <label for="firstname" class="col-md-3 control-label">First Name</label>
-                                <div class="col-md-9">
-                                    <input type="text" class="form-control" name="firstname" placeholder="First Name">
+                                <label for="firstname" class="col-md-4 control-label">Tên bạn</label>
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control" name="firstname" >
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label for="lastname" class="col-md-3 control-label">Last Name</label>
-                                <div class="col-md-9">
-                                    <input type="text" class="form-control" name="lastname" placeholder="Last Name">
+                                <label for="lastname" class="col-md-4 control-label">Họ bạn</label>
+                                <div class="col-md-8">
+                                    <input type="text" class="form-control" name="lastname" >
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label for="email" class="col-md-3 control-label">Email</label>
-                                <div class="col-md-9">
-                                    <input type="email" class="form-control" name="email" placeholder="Email">
+                                <label for="email" class="col-md-4 control-label">Mail công cty</label>
+                                <div class="col-md-8">
+                                    <input type="email" class="form-control" name="email" >
                                 </div>
                             </div>
 
                             <div class="form-group">
-                                <label for="email" class="col-md-3 control-label">Phòng ban</label>
-                                <div class="col-md-9">
+                                <label for="email" class="col-md-4 control-label">Phòng ban</label>
+                                <div class="col-md-8">
                                     <select name="department" id="" class="form-control">
                                        <?php 
                                           $args = array(  'post_type'=> 'department','posts_per_page' => -1 );
@@ -165,8 +103,8 @@
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label for="email" class="col-md-3 control-label">Level</label>
-                                <div class="col-md-9">
+                                <label for="email" class="col-md-4 control-label">Cấp bậc</label>
+                                <div class="col-md-8">
                                     <select name="level" id="" class="form-control">
                                         <option value="1">Non-Manager</option>
                                         <option value="2">Manager</option>
@@ -179,15 +117,15 @@
                             
                             
                             <div class="form-group">
-                                <label for="password" class="col-md-3 control-label">Password</label>
-                                <div class="col-md-9">
-                                    <input type="password" class="form-control" name="passwd" placeholder="Password">
+                                <label for="password" class="col-md-4 control-label">Password</label>
+                                <div class="col-md-8">
+                                    <input type="password" class="form-control" name="passwd" >
                                 </div>
                             </div>
                             <div class="form-group">
-                                <label for="password" class="col-md-3 control-label">Confirm Password</label>
-                                <div class="col-md-9">
-                                    <input type="password" class="form-control" name="re-passwd" placeholder="Password">
+                                <label for="password" class="col-md-4 control-label">Confirm Password</label>
+                                <div class="col-md-8">
+                                    <input type="password" class="form-control" name="re-passwd" >
                                 </div>
                             </div>
 
@@ -195,19 +133,12 @@
 
                             <div class="form-group">
                                 <!-- Button -->                                        
-                                <div class="col-md-offset-3 col-md-9">
-                                    <button  id="btn-signup" type="button" class="btn btn-info" ><i class="icon-hand-right"></i> &nbsp Sign Up</button>
-                                    <span style="margin-left:8px;">or</span>  
+                                <div class="col-md-offset-4 col-md-8">
+                                    <button  id="btn-signup" type="button" class="btn btn-info" ><i class="icon-hand-right"></i> &nbsp Tạo tài khoản</button>
+                                      
                                 </div>
                             </div>
 
-                            <div style="border-top: 1px solid #999; padding-top:20px"  class="form-group">
-
-                                <div class="col-md-offset-3 col-md-9">
-                                    <button id="btn-fbsignup" type="button" class="btn btn-primary"><i class="icon-facebook"></i>   Sign Up with Facebook</button>
-                                </div>                                           
-
-                            </div>
                             <?php wp_nonce_field( 'register-game', 'coca-register-form' ); ?>
 
 
@@ -225,7 +156,15 @@
 
     </main>
     <footer>
-        
+        <div class="info-footer">
+            <div class="col-md-6">
+                <h1 class="title-footer-left"><a href="javascript:void(0)"><img src="<?php print THEME_IMG_URI; ?>logo-footer-left.png" alt=""></a></h1>
+            </div>
+            <div class="col-md-6">
+                <h1 class="title-footer-left"><a href="javascript:void(0)"><img src="<?php print THEME_IMG_URI; ?>logo-footer-right.png" alt=""></a></h1>
+            </div>
+        </div>
+        <a href="javascript:void(0);" class="logo-center"></a>
     </footer>
     <?php wp_footer(); ?>
   </body>
